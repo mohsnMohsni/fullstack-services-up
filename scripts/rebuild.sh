56a1e80ace430d5cbef64dfd5eb7d79f71b9d189
Mohsen Mohseni <mohsenmohseni.co@gmail.com>
2022-12-03 17:22:18 +0330

    refactor: set better command to handle better

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,5 +1,7 @@
-(cd ../pweb-f; git pull origin main);
-(cd ../pweb-b; git pull origin main);
+(cd ../pweb-f || exit; git pull origin main);
+(cd ../pweb-b || exit; git pull origin main);
 git pull origin main;
+
 sudo docker-compose up --build -d;
+
 docker image rm -f $(docker images -q --filter "dangling=true");
