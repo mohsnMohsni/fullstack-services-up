793e6cb4e99beab1762302d0d24c207e9a6a48cf
Mohsen Mohseni <mohsenmohseni.co@gmail.com>
2022-12-10 22:33:07 +0330

    REFACTOR: pass env file on production

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -2,7 +2,7 @@
 (cd ../pweb-b || exit; git pull origin main);
 git pull origin main;
 
-sudo docker-compose up --build -d;
+sudo docker-compose --env-file ./envs/.env.production up --build -d;
 
 docker image rm -f $(docker images -q --filter "dangling=true");
 
